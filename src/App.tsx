2f1ac062c7873640d6ae907b791628f9a9e37408
Avijit Biswas <avijitb440@gmail.com>
2022-07-22 10:10:33 +0530

    babel and webpack setup, project ready

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,50 @@
-import React from "react";
+import React, { FC } from "react";
 import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <Header logo={logo}>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
+        <Link href="https://reactjs.org" target="_blank">
           Learn React
-        </a>
-      </header>
+        </Link>
+      </Header>
     </div>
   );
 }
+
+interface HeaderProps {
+  logo: string;
+  children: any;
+}
+
+const Header: FC<HeaderProps> = ({ children, logo }) => (
+  <header className="App-header">
+    <img src={logo} className="App-logo" alt="logo" />
+    {children}
+  </header>
+);
+
+interface LinkProps {
+  href: string;
+  target?: "_blank" | "_self" | "_parent" | "_top";
+  rel?: string;
+  children: any;
+}
+
+const Link: FC<LinkProps> = ({
+  href = "https://reactjs.org",
+  target = "_blank",
+  rel = "noopener noreferrer",
+  children,
+}) => (
+  <a className="App-link" href={href} target={target} rel={rel}>
+    {children}
+  </a>
+);
 
 export default App;
